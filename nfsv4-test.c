c231eeb3f00efcbce20c01341016cab0a224652a
Shivank Garg <shivankgarg98@gmail.com>
2020-08-14 19:45:01 +0000

    audit lockt,locku,lookup,nverify,open,openattr

--- nfsv4-test.c
+++ nfsv4-test.c
@@ -105,6 +105,7 @@ nfs4_op_close(__unused struct nfs_context *nfs, nfs_argop4 *op, struct nfsfh *fh
 
 	op[0].argop = OP_CLOSE;
 	clargs = &op[0].nfs_argop4_u.opclose;
+	memset(clargs, 0, sizeof(*clargs));
 	clargs->seqid = nfs->seqid;
 	clargs->open_stateid.seqid = fh->stateid.seqid;
 	memcpy(clargs->open_stateid.other, fh->stateid.other, 12);
@@ -118,6 +119,7 @@ nfs4_op_delepurge(struct nfs_context *nfs, nfs_argop4 *op)
 
 	op[0].argop = OP_DELEGPURGE;
 	dpargs = &op[0].nfs_argop4_u.opdelegpurge;
+	memset(dpargs, 0, sizeof(*dpargs));
 	dpargs->clientid = nfs->clientid;
 
 	return 1;
@@ -130,6 +132,7 @@ nfs4_op_delegreturn(__unused struct nfs_context *nfs, nfs_argop4 *op, struct nfs
 
 	op[0].argop = OP_DELEGRETURN;
 	drargs = &op[0].nfs_argop4_u.opdelegreturn;
+	memset(drargs, 0, sizeof(*drargs));
 	drargs->deleg_stateid.seqid = fh->stateid.seqid;
 	memcpy(drargs->deleg_stateid.other, fh->stateid.other, 12);
 
@@ -143,6 +146,7 @@ nfs4_op_putfh(__unused struct nfs_context *nfs, nfs_argop4 *op, struct nfsfh *nf
 
 	op[0].argop = OP_PUTFH;
 	pfargs = &op[0].nfs_argop4_u.opputfh;
+	memset(pfargs, 0, sizeof(*pfargs));
 	pfargs->object.nfs_fh4_len = nfsfh->fh.len;
 	pfargs->object.nfs_fh4_val = nfsfh->fh.val;
 
@@ -158,7 +162,6 @@ nfs4_op_getattr(__unused struct nfs_context *nfs, nfs_argop4 *op,
 	op[0].argop = OP_GETATTR;
 	gaargs = &op[0].nfs_argop4_u.opgetattr;
 	memset(gaargs, 0, sizeof(*gaargs));
-
 	gaargs->attr_request.bitmap4_val = attributes;
 	gaargs->attr_request.bitmap4_len = count;
 
@@ -196,6 +199,7 @@ nfs4_op_lock(struct nfs_context *nfs, nfs_argop4 *op, struct nfsfh *fh,
 
 	op[0].argop = cmd;
 	largs = &op[0].nfs_argop4_u.oplock;
+	memset(largs, 0, sizeof(*largs));
 	largs->locktype = locktype;
 	largs->reclaim  = reclaim;
 	largs->offset   = offset;
@@ -227,6 +231,100 @@ nfs4_op_lock(struct nfs_context *nfs, nfs_argop4 *op, struct nfsfh *fh,
 		    fh->lock_seqid;
 	}
 	fh->lock_seqid++;
+
+	return 1;
+}
+
+static int
+nfs4_op_lockt(struct nfs_context *nfs, nfs_argop4 *op, __unused struct nfsfh *fh,
+              nfs_lock_type4 locktype, uint64_t offset, length4 length)
+{
+	LOCKT4args *ltargs;
+
+	op[0].argop = OP_LOCKT;
+	ltargs = &op[0].nfs_argop4_u.oplockt;
+	memset(ltargs, 0, sizeof(*ltargs));
+	ltargs->locktype = locktype;
+	ltargs->offset   = offset;
+	ltargs->length   = length;
+	ltargs->owner.clientid = nfs->clientid;
+	ltargs->owner.owner.owner_len = strlen(nfs->client_name);
+	ltargs->owner.owner.owner_val = nfs->client_name;
+
+	return 1;
+}
+
+__unused static int
+nfs4_op_locku(__unused struct nfs_context *nfs, nfs_argop4 *op, struct nfsfh *fh,
+              nfs_lock_type4 locktype, uint64_t offset, length4 length)
+{
+	LOCKU4args *luargs;
+
+	op[0].argop = OP_LOCKU;
+	luargs = &op[0].nfs_argop4_u.oplocku;
+	memset(luargs, 0, sizeof(*luargs));
+	luargs->locktype = locktype;
+	luargs->offset   = offset;
+	luargs->length   = length;
+	luargs->seqid = fh->lock_seqid;
+	luargs->lock_stateid.seqid = fh->lock_stateid.seqid;
+	memcpy(luargs->lock_stateid.other, fh->lock_stateid.other, 12);
+	fh->lock_seqid++;
+
+	return 1;
+}
+
+static int
+nfs4_op_lookup(__unused struct nfs_context *nfs, nfs_argop4 *op, char *lpath)
+{
+	LOOKUP4args *largs;
+
+	op[0].argop = OP_LOOKUP;
+	largs = &op[0].nfs_argop4_u.oplookup;
+	memset(largs, 0, sizeof(*largs));
+	largs->objname.utf8string_len = strlen(lpath);
+	largs->objname.utf8string_val = lpath;
+
+	return 1;
+}
+
+static int
+nfs4_op_nverify_chmod(__unused struct nfs_context *nfs,
+    nfs_argop4 *op, void* nvbuf)
+{
+	NVERIFY4args *verifyargs;
+	static uint32_t mask[2] = {0, 1 << (FATTR4_MODE - 32)};
+
+	op[0].argop = OP_NVERIFY;
+	verifyargs = &op[0].nfs_argop4_u.opnverify;
+	memset(verifyargs, 0, sizeof(*verifyargs));
+	verifyargs->obj_attributes.attrmask.bitmap4_len = 2;
+	verifyargs->obj_attributes.attrmask.bitmap4_val = mask;
+	verifyargs->obj_attributes.attr_vals.attrlist4_len = 4;
+	verifyargs->obj_attributes.attr_vals.attrlist4_val = nvbuf;
+
+
+	return 1;
+}
+
+static int
+nfs4_op_open(__unused struct nfs_context *nfs, nfs_argop4 *op, char *opath)
+{
+	OPEN4args *oargs;
+
+	op[0].argop = OP_OPEN;
+	oargs = &op[0].nfs_argop4_u.opopen;
+	memset(oargs, 0, sizeof(*oargs));
+	oargs->seqid = nfs->seqid;
+	oargs->share_access |= OPEN4_SHARE_ACCESS_READ;
+	oargs->share_deny = OPEN4_SHARE_DENY_NONE;
+	oargs->owner.clientid = nfs->clientid;
+	oargs->owner.owner.owner_len = strlen(nfs->client_name);
+	oargs->owner.owner.owner_val = nfs->client_name;
+	oargs->openhow.opentype = OPEN4_NOCREATE;
+	oargs->claim.claim = CLAIM_NULL;
+	oargs->claim.open_claim4_u.file.utf8string_len = strlen(opath);
+	oargs->claim.open_claim4_u.file.utf8string_val = opath;
 
 	return 1;
 }
@@ -792,19 +890,404 @@ ATF_TC_BODY(nfs4_lock_failure, tc)
 
 	struct au_rpc_data au_test_data;
 	int i;
-	nfs_argop4 op[3];
+	nfs_argop4 op[2];
 	struct nfsfh *nfsfh = NULL;
 	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_LOCK, &au_test_data);
 	const char *regex = "nfsrvd_lock.*return,failure";
 
+	/* Invalid argument: length == 0 in lock args result in error. */
 	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDWR, &nfsfh));
 	i = nfs4_op_putfh(nfs, &op[0], nfsfh);
-	i += nfs4_op_lock(nfs, &op[i], nfsfh, OP_LOCK, WRITEW_LT, 0, 0, 1);
-	i += nfs4_op_lock(nfs, &op[i], nfsfh, OP_LOCK, WRITEW_LT, 0, 0, 1);
+	i += nfs4_op_lock(nfs, &op[i], nfsfh, OP_LOCK, WRITEW_LT, 0, 0, 0);
 	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
 }
 
 ATF_TC_CLEANUP(nfs4_lock_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_lockt_success);
+ATF_TC_HEAD(nfs4_lockt_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 lockt sub-op");
+}
+
+ATF_TC_BODY(nfs4_lockt_success, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[2];
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_LOCKT, &au_test_data);
+	const char *regex = "nfsrvd_lockt.*return,success";
+
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDWR, &nfsfh));
+	i = nfs4_op_putfh(nfs, &op[0], nfsfh);
+	i += nfs4_op_lockt(nfs, &op[i], nfsfh, WRITEW_LT, 0, 1);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, true);
+}
+
+ATF_TC_CLEANUP(nfs4_lockt_success, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_lockt_failure);
+ATF_TC_HEAD(nfs4_lockt_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
+					"NFSv4 lockt sub-op");
+}
+
+ATF_TC_BODY(nfs4_lockt_failure, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[3];
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_LOCKT, &au_test_data);
+	const char *regex = "nfsrvd_lockt.*return,failure";
+
+	/* Invalid argument: length == 0 in lockt args result in error. */
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDWR, &nfsfh));
+	i = nfs4_op_putfh(nfs, &op[0], nfsfh);
+	i += nfs4_op_lockt(nfs, &op[i], nfsfh, WRITEW_LT, 0, 0);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
+}
+
+ATF_TC_CLEANUP(nfs4_lockt_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_locku_success);
+ATF_TC_HEAD(nfs4_locku_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 locku sub-op");
+}
+
+ATF_TC_BODY(nfs4_locku_success, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_LOCKU, &au_test_data);
+	const char *regex = "nfsrvd_locku.*return,success";
+
+	/*
+	 * For some unknown reason, nfsv4-raw-api sub-op locku fails.
+	 * Hence, Used high-level API.
+	 */
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDWR, &nfsfh));
+/**/	ATF_REQUIRE_EQ(0, nfs_lockf(nfs, nfsfh, NFS4_F_LOCK, 1));
+/**/	FILE *pipefd = setup(fds, auclass);
+/**/	ATF_REQUIRE_EQ(0, nfs_lockf(nfs, nfsfh, NFS4_F_ULOCK, 1));
+/*	int i;
+	nfs_argop4 op[4];
+	i = nfs4_op_putfh(nfs, &op[0], nfsfh);
+	i += nfs4_op_lock(nfs, &op[i], nfsfh, OP_LOCK, READW_LT, 0, 0, 1);
+	i += nfs4_op_putfh(nfs, &op[i], nfsfh);
+	i += nfs4_op_locku(nfs, &op[i], nfsfh, READW_LT, 0, 1);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, true);
+*//**/	check_audit(fds, (regex), pipefd);	
+}
+
+ATF_TC_CLEANUP(nfs4_locku_success, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_locku_failure);
+ATF_TC_HEAD(nfs4_locku_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
+					"NFSv4 locku sub-op");
+}
+
+ATF_TC_BODY(nfs4_locku_failure, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_LOCKU, &au_test_data);
+	const char *regex = "nfsrvd_locku.*return,failure";
+
+	/* Area not locked, resulting in unsuccessful locku operation. */
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDWR, &nfsfh));
+	/*ATF_REQUIRE_EQ(0, nfs_lockf(nfs, nfsfh, NFS4_F_LOCK, 1));*/
+	FILE *pipefd = setup(fds, auclass);
+	ATF_REQUIRE(nfs_lockf(nfs, nfsfh, NFS4_F_ULOCK, 1) != 0);
+
+	check_audit(fds, regex, pipefd);
+}
+
+ATF_TC_CLEANUP(nfs4_locku_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_lookup_success);
+ATF_TC_HEAD(nfs4_lookup_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 lookup sub-op");
+}
+
+ATF_TC_BODY(nfs4_lookup_success, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[2];
+	struct nfsfh dirfh;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_LOOKUP, &au_test_data);
+	const char *regex = "nfsrvd_lookup.*return,success";
+
+	dirfh.fh.len = nfs->rootfh.len;
+	dirfh.fh.val = nfs->rootfh.val;
+	i = nfs4_op_putfh(nfs, &op[0], &dirfh);
+	i += nfs4_op_lookup(nfs, &op[i], path);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, true);
+}
+
+ATF_TC_CLEANUP(nfs4_lookup_success, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_lookup_failure);
+ATF_TC_HEAD(nfs4_lookup_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 lookup sub-op");
+}
+
+ATF_TC_BODY(nfs4_lookup_failure, tc)
+{
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[2];
+	struct nfsfh dirfh;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_LOOKUP, &au_test_data);
+	const char *regex = "nfsrvd_lookup.*return,failure";
+
+	/* No such file or directory with name fileforaudit. */
+	dirfh.fh.len = nfs->rootfh.len;
+	dirfh.fh.val = nfs->rootfh.val;
+	i = nfs4_op_putfh(nfs, &op[0], &dirfh);
+	i += nfs4_op_lookup(nfs, &op[i], path);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
+}
+
+ATF_TC_CLEANUP(nfs4_lookup_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_lookupp_success);
+ATF_TC_HEAD(nfs4_lookupp_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 lookupp sub-op");
+}
+
+ATF_TC_BODY(nfs4_lookupp_success, tc)
+{
+	/* 
+	 * Do libnfs has complete support for OP_LOOKUPP.
+	 * There is no "LOOKUPP4args" for lookupp, but there is
+	 * LOOKUPP4res. This left me confused on how to write a
+	 * test for it.
+	 */
+	ATF_REQUIRE_EQ_MSG(1, 0, "TODO: more details in comment");
+}
+
+ATF_TC_CLEANUP(nfs4_lookupp_success, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_lookupp_failure);
+ATF_TC_HEAD(nfs4_lookupp_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 lookupp sub-op");
+}
+
+ATF_TC_BODY(nfs4_lookupp_failure, tc)
+{
+	/* 
+	 * Do libnfs has complete support for OP_LOOKUPP.
+	 * There is no "LOOKUPP4args" for lookupp, but there is
+	 * LOOKUPP4res. This left me confused on how to write a
+	 * test for it.
+	 */
+
+	ATF_REQUIRE_EQ_MSG(1, 0, "TODO: more details in comment");
+}
+
+ATF_TC_CLEANUP(nfs4_lookupp_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_nverify_success);
+ATF_TC_HEAD(nfs4_nverify_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 nverify sub-op");
+}
+
+ATF_TC_BODY(nfs4_nverify_success, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[2];
+	struct nfsfh *nfsfh = NULL;
+	uint32_t m = S_IFREG | S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_NVERIFY, &au_test_data);
+	const char *regex = "nfsrvd_verify.*return,success";
+
+	m = htonl(m);
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDONLY, &nfsfh));
+	i = nfs4_op_putfh(nfs, &op[0], nfsfh);
+	i += nfs4_op_nverify_chmod(nfs, &op[i], &m);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, true);
+}
+
+ATF_TC_CLEANUP(nfs4_nverify_success, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_nverify_failure);
+ATF_TC_HEAD(nfs4_nverify_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
+					"NFSv4 nverify sub-op");
+}
+
+ATF_TC_BODY(nfs4_nverify_failure, tc)
+{
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[1];
+	uint32_t m = S_IFREG | S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_NVERIFY, &au_test_data);
+	const char *regex = "nfsrvd_verify.*return,failure";
+
+	/* NFSv4 NVERIFY sub-operation will fail due to invalid use. (no PUTFH subop) */
+	m = htonl(m);
+	i = nfs4_op_nverify_chmod(nfs, &op[0], &m);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
+}
+
+ATF_TC_CLEANUP(nfs4_nverify_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_open_success);
+ATF_TC_HEAD(nfs4_open_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 open sub-op");
+}
+
+ATF_TC_BODY(nfs4_open_success, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[2];
+	struct nfsfh dirfh;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_OPEN, &au_test_data);
+	const char *regex = "nfsrvd_open.*return,success";
+
+	dirfh.fh.len = nfs->rootfh.len;
+	dirfh.fh.val = nfs->rootfh.val;
+	i = nfs4_op_putfh(nfs, &op[0], &dirfh);
+	i += nfs4_op_open(nfs, &op[i], path);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, true);
+}
+
+ATF_TC_CLEANUP(nfs4_open_success, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_open_failure);
+ATF_TC_HEAD(nfs4_open_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
+					"NFSv4 open sub-op");
+}
+
+ATF_TC_BODY(nfs4_open_failure, tc)
+{
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[2];
+	struct nfsfh dirfh;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_OPEN, &au_test_data);
+	const char *regex = "nfsrvd_open.*return,failure";
+
+	/* Open type is OPEN4_NOCREATE and no file exists with name path. */
+	dirfh.fh.len = nfs->rootfh.len;
+	dirfh.fh.val = nfs->rootfh.val;
+	i = nfs4_op_putfh(nfs, &op[0], &dirfh);
+	i += nfs4_op_open(nfs, &op[i], path);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
+}
+
+ATF_TC_CLEANUP(nfs4_open_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_openattr_failure);
+ATF_TC_HEAD(nfs4_openattr_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
+					"NFSv4 openattr sub-op");
+}
+
+ATF_TC_BODY(nfs4_openattr_failure, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[2];
+	struct nfsfh dirfh;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_OPENATTR, &au_test_data);
+	const char *regex = "nfsrvd_openattr.*text,NFSv4 service not supported";
+
+	dirfh.fh.len = nfs->rootfh.len;
+	dirfh.fh.val = nfs->rootfh.val;
+	i = nfs4_op_putfh(nfs, &op[0], &dirfh);
+	OPENATTR4args *oaargs;
+	oaargs = &op[i].nfs_argop4_u.opopenattr;
+	memset(oaargs, 0, sizeof(*oaargs));
+	op[i++].argop = OP_OPENATTR;
+	oaargs->createdir = true;
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
+}
+
+ATF_TC_CLEANUP(nfs4_openattr_failure, tc)
 {
 	cleanup();
 }
@@ -831,22 +1314,21 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, nfs4_link_failure);
 	ATF_TP_ADD_TC(tp, nfs4_lock_success);
 	ATF_TP_ADD_TC(tp, nfs4_lock_failure);
-//	ATF_TP_ADD_TC(tp, nfs4_lockt_success);
-//	ATF_TP_ADD_TC(tp, nfs4_lockt_failure);
-//	ATF_TP_ADD_TC(tp, nfs4_locku_success);
-//	ATF_TP_ADD_TC(tp, nfs4_locku_failure);
-//	ATF_TP_ADD_TC(tp, nfs4_lookup_success);
-//	ATF_TP_ADD_TC(tp, nfs4_lookup_failure);
-//	ATF_TP_ADD_TC(tp, nfs4_lookupp_success);
-//      ATF_TP_ADD_TC(tp, nfs4_lookupp_failure);
-//	ATF_TP_ADD_TC(tp, nfs4_nverify_success);
-//      ATF_TP_ADD_TC(tp, nfs4_nverify_failure);
-//    	ATF_TP_ADD_TC(tp, nfs4_open_success);
-//      ATF_TP_ADD_TC(tp, nfs4_open_failure);
-//	ATF_TP_ADD_TC(tp, nfs4_openattr_success);
-//      ATF_TP_ADD_TC(tp, nfs4_openattr_failure);
+	ATF_TP_ADD_TC(tp, nfs4_lockt_success);
+	ATF_TP_ADD_TC(tp, nfs4_lockt_failure);
+	ATF_TP_ADD_TC(tp, nfs4_locku_success); /* Used high-level nfs4 api. */
+	ATF_TP_ADD_TC(tp, nfs4_locku_failure); /* Used high-level nfs4 api. */
+	ATF_TP_ADD_TC(tp, nfs4_lookup_success);
+	ATF_TP_ADD_TC(tp, nfs4_lookup_failure);
+	ATF_TP_ADD_TC(tp, nfs4_lookupp_success); /* TODO: read comment in tc_body */
+	ATF_TP_ADD_TC(tp, nfs4_lookupp_failure); /* TODO: read comment in tc_body  */
+	ATF_TP_ADD_TC(tp, nfs4_nverify_success);
+	ATF_TP_ADD_TC(tp, nfs4_nverify_failure);
+    	ATF_TP_ADD_TC(tp, nfs4_open_success);
+	ATF_TP_ADD_TC(tp, nfs4_open_failure);
+	ATF_TP_ADD_TC(tp, nfs4_openattr_failure); /* NFSv4 service not supported */
 //	ATF_TP_ADD_TC(tp, nfs4_openconfirm_success);
-//      ATF_TP_ADD_TC(tp, nfs4_openconfirm_failure);
+//	ATF_TP_ADD_TC(tp, nfs4_openconfirm_failure);
 //	ATF_TP_ADD_TC(tp, nfs4_opendowngrade_success);
 //      ATF_TP_ADD_TC(tp, nfs4_opendowngrade_failure);
 //	ATF_TP_ADD_TC(tp, nfs4_putfh_success);
