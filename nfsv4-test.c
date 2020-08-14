779bb7078ad7f6069a27ca1a5c6c43aa417a1ee2
Shivank Garg <shivankgarg98@gmail.com>
2020-08-14 06:28:25 +0000

    audit v4 close commit create

--- nfsv4-test.c
+++ nfsv4-test.c
@@ -73,22 +73,43 @@ nfs4_op_commit(__unused struct nfs_context *nfs, nfs_argop4 *op)
 }
 
 static int
+nfs4_op_create_char(__unused struct nfs_context *nfs, nfs_argop4 *op)
+{
+	CREATE4args *cargs;
+	uint32_t attrmask[2];
+	uint32_t attr_vals[1];
+
+	op[0].argop = OP_CREATE;
+	cargs = &op[0].nfs_argop4_u.opcreate;
+	memset(cargs, 0, sizeof(*cargs));
+	cargs->objname.utf8string_len = strlen(path);
+	cargs->objname.utf8string_val = path;
+	attrmask[0] = 0;
+	attrmask[1] = 1 << (FATTR4_MODE - 32);
+	cargs->createattrs.attrmask.bitmap4_len = 2;
+	cargs->createattrs.attrmask.bitmap4_val = attrmask;
+	attr_vals[0] = S_IFMT | S_IFCHR;
+	cargs->createattrs.attr_vals.attrlist4_len = 4;
+	cargs->createattrs.attr_vals.attrlist4_val = (char *)attr_vals;
+	cargs->objtype.type = NF4CHR;
+	cargs->objtype.createtype4_u.devdata.specdata1 = 1;
+	cargs->objtype.createtype4_u.devdata.specdata2 = 1;
+
+	return 1;
+}
+
+static int
 nfs4_op_close(__unused struct nfs_context *nfs, nfs_argop4 *op, struct nfsfh *fh)
 {
 	CLOSE4args *clargs;
-	int i = 0;
 
-	if (fh->is_dirty) {
-	        i += nfs4_op_commit(nfs, &op[i]);
-	}
-
-	op[i].argop = OP_CLOSE;
-	clargs = &op[i++].nfs_argop4_u.opclose;
+	op[0].argop = OP_CLOSE;
+	clargs = &op[0].nfs_argop4_u.opclose;
 	clargs->seqid = nfs->seqid;
 	clargs->open_stateid.seqid = fh->stateid.seqid;
 	memcpy(clargs->open_stateid.other, fh->stateid.other, 12);
 
-	return i;
+	return 1;
 }
 
 static int
@@ -124,7 +145,7 @@ ATF_TC_WITH_CLEANUP(nfs4_access_success);
 ATF_TC_HEAD(nfs4_access_success, tc)
 {
 	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
-					"NFSv4 Access RPC");
+					"NFSv4 Access sub-op");
 }
 
 ATF_TC_BODY(nfs4_access_success, tc)
@@ -153,7 +174,7 @@ ATF_TC_WITH_CLEANUP(nfs4_access_failure);
 ATF_TC_HEAD(nfs4_access_failure, tc)
 {
 	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
-					"NFSv4 Access RPC");
+					"NFSv4 Access sub-op");
 }
 
 ATF_TC_BODY(nfs4_access_failure, tc)
@@ -178,7 +199,7 @@ ATF_TC_WITH_CLEANUP(nfs4_close_success);
 ATF_TC_HEAD(nfs4_close_success, tc)
 {
 	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
-					"NFSv4 close RPC");
+					"NFSv4 close sub-op");
 }
 
 ATF_TC_BODY(nfs4_close_success, tc)
@@ -203,11 +224,147 @@ ATF_TC_CLEANUP(nfs4_close_success, tc)
 	cleanup();
 }
 
+ATF_TC_WITH_CLEANUP(nfs4_close_failure);
+ATF_TC_HEAD(nfs4_close_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
+					"NFSv4 close sub-op");
+}
+
+ATF_TC_BODY(nfs4_close_failure, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[1];
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_CLOSE, &au_test_data);
+	const char *regex = "nfsrvd_close.*return,failure";
+
+	/* File removed before making sub-op call, stale file handle. */
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDONLY, &nfsfh));
+	ATF_REQUIRE_EQ(0, remove(path));
+	i = nfs4_op_close(nfs, &op[0], nfsfh);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
+}
+
+ATF_TC_CLEANUP(nfs4_close_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_commit_success);
+ATF_TC_HEAD(nfs4_commit_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 commit sub-op");
+}
+
+ATF_TC_BODY(nfs4_commit_success, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[2];
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_COMMIT, &au_test_data);
+	const char *regex = "nfsrvd_commit.*return,success";
+
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDONLY, &nfsfh));
+	i = nfs4_op_putfh(nfs, &op[0], nfsfh);
+	i += nfs4_op_commit(nfs, &op[i]);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, true);
+}
+
+ATF_TC_CLEANUP(nfs4_commit_success, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_commit_failure);
+ATF_TC_HEAD(nfs4_commit_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
+					"NFSv4 commit sub-op");
+}
+
+ATF_TC_BODY(nfs4_commit_failure, tc)
+{
+	struct au_rpc_data au_test_data;
+	int i;
+	nfs_argop4 op[1];
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_COMMIT, &au_test_data);
+	const char *regex = "nfsrvd_commit.*return,failure";
+
+	/* NFSv4 COMMIT sub-operation will fail due to invalid use. (no PUTFH subop) */
+	i = nfs4_op_commit(nfs, &op[0]);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
+}
+
+ATF_TC_CLEANUP(nfs4_commit_failure, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_create_success);
+ATF_TC_HEAD(nfs4_create_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 create sub-op");
+}
+
+ATF_TC_BODY(nfs4_create_success, tc)
+{
+	struct au_rpc_data au_test_data;
+	nfs_argop4 op[2];
+	int i;
+	struct nfsfh nfsfh;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_CREATE, &au_test_data);
+	const char *regex = "nfsrvd_mknod.*return,success";
+
+	(&nfsfh)->fh = nfs->rootfh;
+	i = nfs4_op_putfh(nfs, &op[0], &nfsfh);
+	i += nfs4_op_create_char(nfs, &op[i]);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, true);
+}
+
+ATF_TC_CLEANUP(nfs4_create_success, tc)
+{
+	cleanup();
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_create_failure);
+ATF_TC_HEAD(nfs4_create_failure, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
+					"NFSv4 create sub-op");
+}
+
+ATF_TC_BODY(nfs4_create_failure, tc)
+{
+	struct au_rpc_data au_test_data;
+	nfs_argop4 op[1];
+	int i;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_CREATE, &au_test_data);
+	const char *regex = "nfsrvd_mknod.*return,failure";
+
+	/* NFSv4 CREATE sub-op will fail due to invalid use. (no PUTFH subop) */
+	i = nfs4_op_create_char(nfs, &op[0]);
+	NFS4_COMMON_PERFORM(i, op, regex, nfs, au_test_data, false);
+}
+
+ATF_TC_CLEANUP(nfs4_create_failure, tc)
+{
+	cleanup();
+}
+
 ATF_TC_WITH_CLEANUP(nfs4_getattr_success);
 ATF_TC_HEAD(nfs4_getattr_success, tc)
 {
 	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
-					"NFSv4 getattr RPC");
+					"NFSv4 getattr sub-op");
 }
 
 ATF_TC_BODY(nfs4_getattr_success, tc)
@@ -236,7 +393,7 @@ ATF_TC_WITH_CLEANUP(nfs4_getattr_failure);
 ATF_TC_HEAD(nfs4_getattr_failure, tc)
 {
 	atf_tc_set_md_var(tc, "descr", "Tests the audit of an unsuccessful "
-					"NFSv4 getattr RPC");
+					"NFSv4 getattr sub-op");
 }
 
 ATF_TC_BODY(nfs4_getattr_failure, tc)
@@ -262,7 +419,11 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, nfs4_access_success);
 	ATF_TP_ADD_TC(tp, nfs4_access_failure);
 	ATF_TP_ADD_TC(tp, nfs4_close_success);
-//	ATF_TP_ADD_TC(tp, nfs4_close_failure);
+	ATF_TP_ADD_TC(tp, nfs4_close_failure);
+	ATF_TP_ADD_TC(tp, nfs4_commit_success);
+	ATF_TP_ADD_TC(tp, nfs4_commit_failure);
+	ATF_TP_ADD_TC(tp, nfs4_create_success);
+	ATF_TP_ADD_TC(tp, nfs4_create_failure);
 	ATF_TP_ADD_TC(tp, nfs4_getattr_success);
 	ATF_TP_ADD_TC(tp, nfs4_getattr_failure);
 
