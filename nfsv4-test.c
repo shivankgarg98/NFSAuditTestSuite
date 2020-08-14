2a20a14f482cbb48f221317341d2ad800020b2cc
Shivank Garg <shivankgarg98@gmail.com>
2020-08-14 00:14:44 +0000

    add nfsv4 getattr failure

--- nfsv4-test.c
+++ nfsv4-test.c
@@ -29,6 +29,19 @@ static char path[] = "fileforaudit";
 //static const char *failurereg = "fileforaudit.*return,failure";
 
 static int
+nfs4_op_access(__unused struct nfs_context *nfs, nfs_argop4 *op, uint32_t access_mask)
+{
+        ACCESS4args *aargs;
+
+        op[0].argop = OP_ACCESS;
+        aargs = &op[0].nfs_argop4_u.opaccess;
+        memset(aargs, 0, sizeof(*aargs));
+        aargs->access = access_mask;
+
+        return 1;
+}
+
+static int
 nfs4_op_putfh(__unused struct nfs_context *nfs, nfs_argop4 *op, struct nfsfh *nfsfh)
 {
 	PUTFH4args *pfargs;
@@ -55,6 +68,45 @@ nfs4_op_getattr(__unused struct nfs_context *nfs, nfs_argop4 *op,
 	gaargs->attr_request.bitmap4_len = count;
 
 	return 1;
+}
+
+ATF_TC_WITH_CLEANUP(nfs4_access_success);
+ATF_TC_HEAD(nfs4_access_success, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests the audit of a successful "
+					"NFSv4 Access RPC");
+}
+
+ATF_TC_BODY(nfs4_access_success, tc)
+{
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	FILE *pipefd;
+	int i;
+	COMPOUND4args args;
+	nfs_argop4 op[1];
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_ACCESS, &au_test_data);
+	const char *regex = "nfsrvd_access.*return,success";
+
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDONLY, &nfsfh));
+	pipefd = setup(fds, auclass);
+	i = nfs4_op_access(nfs, &op[0], ACCESS4_READ);
+	memset(&args, 0, sizeof(args));
+	args.argarray.argarray_len = i;
+	args.argarray.argarray_val = op;
+	printf("%ld\n",nfs->clientid);
+	ATF_REQUIRE_EQ(0, rpc_nfs4_compound_async(nfs->rpc,
+	    (rpc_cb)nfsv4_res_close_cb, &args, &au_test_data));
+	ATF_REQUIRE_EQ(RPC_STATUS_SUCCESS, nfs_poll_fd(nfs, &au_test_data));
+	ATF_REQUIRE_EQ(NFS3_OK, au_test_data.au_rpc_result);
+	check_audit(fds, regex, pipefd);
+}
+
+ATF_TC_CLEANUP(nfs4_access_success, tc)
+{
+	cleanup();
 }
 
 ATF_TC_WITH_CLEANUP(nfs4_getattr_success);
@@ -84,7 +136,8 @@ ATF_TC_BODY(nfs4_getattr_success, tc)
 	memset(&args, 0, sizeof(args));
 	args.argarray.argarray_len = i;
 	args.argarray.argarray_val = op;
-	ATF_REQUIRE_EQ(0, rpc_nfs4_compound_async(nfs->rpc, (rpc_cb)nfsv4_res_close_cb, &args, &au_test_data));
+	ATF_REQUIRE_EQ(0, rpc_nfs4_compound_async(nfs->rpc,
+	    (rpc_cb)nfsv4_res_close_cb, &args, &au_test_data));
 	ATF_REQUIRE_EQ(RPC_STATUS_SUCCESS, nfs_poll_fd(nfs, &au_test_data));
 	ATF_REQUIRE_EQ(NFS3_OK, au_test_data.au_rpc_result);
 	check_audit(fds, regex, pipefd);
@@ -104,6 +157,29 @@ ATF_TC_HEAD(nfs4_getattr_failure, tc)
 
 ATF_TC_BODY(nfs4_getattr_failure, tc)
 {
+	ATF_REQUIRE(open(path, O_CREAT, 0777) != -1);
+
+	struct au_rpc_data au_test_data;
+	FILE *pipefd;
+	int i;
+	COMPOUND4args args;
+	nfs_argop4 op[1];
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_context *nfs = tc_body_init(AUE_NFSV4OP_GETATTR, &au_test_data);
+	const char *regex = "nfsrvd_getattr.*return,failure";
+
+	ATF_REQUIRE_EQ(0, nfs_open(nfs, path, O_RDONLY, &nfsfh));
+	pipefd = setup(fds, auclass);
+	/* GETATTR sub-operations will fail due to invalid getattr args. (no PUTFH) */
+	i = nfs4_op_getattr(nfs, &op[0], standard_attributes, 2);
+	memset(&args, 0, sizeof(args));
+	args.argarray.argarray_len = i;
+	args.argarray.argarray_val = op;
+	ATF_REQUIRE_EQ(0, rpc_nfs4_compound_async(nfs->rpc,
+	    (rpc_cb)nfsv4_res_close_cb, &args, &au_test_data));
+	ATF_REQUIRE_EQ(RPC_STATUS_SUCCESS, nfs_poll_fd(nfs, &au_test_data));
+	ATF_REQUIRE(NFS3_OK != au_test_data.au_rpc_result);
+	check_audit(fds, regex, pipefd);
 }
 
 ATF_TC_CLEANUP(nfs4_getattr_failure, tc)
@@ -115,6 +191,7 @@ ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, nfs4_getattr_success);
 	ATF_TP_ADD_TC(tp, nfs4_getattr_failure);
+	ATF_TP_ADD_TC(tp, nfs4_access_success);
 
 	return (atf_no_error());
 }
